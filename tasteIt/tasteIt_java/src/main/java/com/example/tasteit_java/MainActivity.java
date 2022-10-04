70bba45b18658bbba77dffd1a7a13c8817e2726f
Daniel Campos <daniel.campostello@gmail.com>
2022-10-04 15:01:37 +0200

    pasado a java

---
+++ tasteIt/tasteIt_java/src/main/java/com/example/tasteit_java/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.tasteit_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void callSignOut(View view){
+        signOut();
+    }
+    private void signOut(){
+        FirebaseAuth.getInstance().signOut();
+        startActivity (new Intent(this, LoginActivity.class));
+    }
+}
---
+++ tasteIt/tasteIt_java/src/main/java/com/example/tasteit_java/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.tasteit_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
