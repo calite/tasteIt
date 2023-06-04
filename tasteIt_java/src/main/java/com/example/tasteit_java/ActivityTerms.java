a525bc1ab82ba583eb46f647c870fa6b1e49f47b
Pr0tan <arguzmanp@gmail.com>
2023-06-04 10:54:22 +0200

    Terms & Conditions V3

--- tasteIt_java/src/main/java/com/example/tasteit_java/ActivityTerms.java
+++ tasteIt_java/src/main/java/com/example/tasteit_java/ActivityTerms.java
@@ -3,14 +3,24 @@ package com.example.tasteit_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.tasteit_java.R;
 
 public class ActivityTerms extends AppCompatActivity {
-
+    TextView tvterms1;
+    TextView tvterms2;
+    TextView tvterms3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms);
+
+        tvterms1 = findViewById(R.id.tvterms1);
+        tvterms2 = findViewById(R.id.tvterms2);
+        tvterms3 = findViewById(R.id.tvterms3);
+        tvterms1.setText(R.string.terms1);
+        tvterms2.setText(R.string.terms2);
+        tvterms3.setText(R.string.terms3);
     }
 }
